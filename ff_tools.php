3fdddcc9125878e40347d6dc7873c7f334dfde64
Mr. Meo <20571336+trananhmanh89@users.noreply.github.com>
2020-12-16 15:04:25 +0700

    add inline style

--- ff_tools.php
+++ ff_tools.php
@@ -9,6 +9,10 @@ defined('_JEXEC') or die('Restricted access');
 
 class PlgSystemFf_tools extends CMSPlugin
 {
+    protected $_scripts = array();
+    protected $_script = array();
+    protected $_styleSheets = array();
+
     public function onAfterInitialise()
     {
         FFToolsHelper::redirect($this->params);
@@ -21,16 +25,27 @@ class PlgSystemFf_tools extends CMSPlugin
             return;
         }
 
+        $doc = Factory::getDocument();
+
         if ($this->params->get('minify-js')) {
             FFToolsHelper::minifyJS($this->params->get('js-exclude'));
         }
 
         if ($this->params->get('minify-css')) {
             FFToolsHelper::minifyCSS($this->params->get('css-exclude'));
+
+            if ($this->params->get('inline-css')) {
+                foreach ($doc->_styleSheets as $key => $value) {
+                    if (!preg_match('/cache\/ff_tools_css_cache\//', $key)) {
+                        continue;
+                    }
+
+                    $this->_styleSheets[] = $key;
+                }
+            }
         }
 
         if ($this->params->get('script-to-bottom')) {
-            $doc = Factory::getDocument();
             $this->_scripts = $doc->_scripts;
             $this->_script = $doc->_script;
 
@@ -46,114 +61,12 @@ class PlgSystemFf_tools extends CMSPlugin
             return;
         }
 
-        if (!$this->params->get('script-to-bottom')) {
-            return;
+        if ($this->params->get('minify-css') && $this->params->get('inline-css')) {
+            FFToolsHelper::inlineCss($this->_styleSheets);
         }
 
-        $document = Factory::getDocument();
-        $body = $app->getBody();
-
-        $lnEnd        = $document->_getLineEnd();
-        $tab          = $document->_getTab();
-        $buffer       = '';
-        $mediaVersion = $document->getMediaVersion();
-        $defaultJsMimes         = array('text/javascript', 'application/javascript', 'text/x-javascript', 'application/x-javascript');
-        $html5NoValueAttributes = array('defer', 'async');
-        // Generate script file links
-        foreach ($this->_scripts as $src => $attribs) {
-            // Check if script uses IE conditional statements.
-            $conditional = isset($attribs['options']) && isset($attribs['options']['conditional']) ? $attribs['options']['conditional'] : null;
-
-            // Check if script uses media version.
-            if (
-                isset($attribs['options']['version']) && $attribs['options']['version'] && strpos($src, '?') === false
-                && ($mediaVersion || $attribs['options']['version'] !== 'auto')
-            ) {
-                $src .= '?' . ($attribs['options']['version'] === 'auto' ? $mediaVersion : $attribs['options']['version']);
-            }
-
-            $buffer .= $tab;
-
-            // This is for IE conditional statements support.
-            if (!is_null($conditional)) {
-                $buffer .= '<!--[if ' . $conditional . ']>';
-            }
-
-            $buffer .= '<script src="' . $src . '"';
-
-            // Add script tag attributes.
-            foreach ($attribs as $attrib => $value) {
-                // Don't add the 'options' attribute. This attribute is for internal use (version, conditional, etc).
-                if ($attrib === 'options') {
-                    continue;
-                }
-
-                // Don't add type attribute if document is HTML5 and it's a default mime type. 'mime' is for B/C.
-                if (in_array($attrib, array('type', 'mime')) && $document->isHtml5() && in_array($value, $defaultJsMimes)) {
-                    continue;
-                }
-
-                // B/C: If defer and async is false or empty don't render the attribute.
-                if (in_array($attrib, array('defer', 'async')) && !$value) {
-                    continue;
-                }
-
-                // Don't add type attribute if document is HTML5 and it's a default mime type. 'mime' is for B/C.
-                if ($attrib === 'mime') {
-                    $attrib = 'type';
-                }
-                // B/C defer and async can be set to yes when using the old method.
-                elseif (in_array($attrib, array('defer', 'async')) && $value === true) {
-                    $value = $attrib;
-                }
-
-                // Add attribute to script tag output.
-                $buffer .= ' ' . htmlspecialchars($attrib, ENT_COMPAT, 'UTF-8');
-
-                if (!($document->isHtml5() && in_array($attrib, $html5NoValueAttributes))) {
-                    // Json encode value if it's an array.
-                    $value = !is_scalar($value) ? json_encode($value) : $value;
-
-                    $buffer .= '="' . htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '"';
-                }
-            }
-
-            $buffer .= '></script>';
-
-            // This is for IE conditional statements support.
-            if (!is_null($conditional)) {
-                $buffer .= '<![endif]-->';
-            }
-
-            $buffer .= $lnEnd;
+        if ($this->params->get('script-to-bottom')) {
+            FFToolsHelper::moveScriptToBottom($this->_scripts, $this->_script);
         }
-
-        // Generate script declarations
-        foreach ($this->_script as $type => $content) {
-            $buffer .= $tab . '<script';
-
-            if (!is_null($type) && (!$document->isHtml5() || !in_array($type, $defaultJsMimes))) {
-                $buffer .= ' type="' . $type . '"';
-            }
-
-            $buffer .= '>' . $lnEnd;
-
-            // This is for full XHTML support.
-            if ($document->_mime != 'text/html') {
-                $buffer .= $tab . $tab . '//<![CDATA[' . $lnEnd;
-            }
-
-            $buffer .= $content . $lnEnd;
-
-            // See above note
-            if ($document->_mime != 'text/html') {
-                $buffer .= $tab . $tab . '//]]>' . $lnEnd;
-            }
-
-            $buffer .= $tab . '</script>' . $lnEnd;
-        }
-
-        $body = str_replace('</body>', $buffer . '</body>', $body);
-        $app->setBody($body);
     }
 }
--- helper.php
+++ helper.php
@@ -7,7 +7,8 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\Uri\Uri;
 
-class FFToolsHelper {
+class FFToolsHelper
+{
     public static function redirect($params)
     {
         $app = Factory::getApplication();
@@ -82,7 +83,7 @@ class FFToolsHelper {
         if ($remove === 'admin' && !$app->isClient('administrator')) {
             return;
         }
-        
+
         self::overrideBehavior();
     }
 
@@ -131,7 +132,7 @@ class FFToolsHelper {
                     $cache .= $minifier->minify();
                     $cache .= ";\n";
                 }
-                
+
                 if (File::write($cachePath, $cache)) {
                     $doc->addScript($cacheUrl);
                 }
@@ -142,7 +143,7 @@ class FFToolsHelper {
     protected static function getJsChunk($exclude)
     {
         $exclude = explode("\n", $exclude);
-        $exclude = array_filter($exclude, function($value) {
+        $exclude = array_filter($exclude, function ($value) {
             return $value ? trim($value) : false;
         });
         $exclude = array_map('trim', $exclude);
@@ -176,7 +177,8 @@ class FFToolsHelper {
             if ((isset($script['defer']) || isset($script['async']))
                 || (isset($script['options']['conditional']))
                 || ($startWithHttp && !$isAbsUrl)
-                || (!is_file(JPATH_ROOT . $file) && !$isAbsUrl)) {
+                || (!is_file(JPATH_ROOT . $file) && !$isAbsUrl)
+            ) {
                 if ($bundle) {
                     $chunk[] = $bundle;
                     $bundle = array();
@@ -231,7 +233,7 @@ class FFToolsHelper {
                     $output = self::_minifyCss($content);
                     $output = self::correctCssUrlPath(JPATH_ROOT . $path, $output);
                     $output = self::correctCssImportPath(JPATH_ROOT . $path, $output);
-                    
+
                     $cache .= "/* $file */ ";
                     $cache .= $output;
                     $cache .= "\n";
@@ -248,17 +250,17 @@ class FFToolsHelper {
 
     protected static function _minifyCss($css)
     {
-        $css = preg_replace( '~\s+~', ' ', $css );
-		$css = preg_replace( '~/\*.*?\*/~s', '', $css );
-		$css = str_replace( '; ', ';', $css );
-		$css = str_replace( ': ', ':', $css );
-		$css = str_replace( ' {', '{', $css );
-		$css = str_replace( '{ ', '{', $css );
-		$css = str_replace( ', ', ',', $css );
-		$css = str_replace( '} ', '}', $css );
-		$css = str_replace( ';}', '}', $css );
+        $css = preg_replace('~\s+~', ' ', $css);
+        $css = preg_replace('~/\*.*?\*/~s', '', $css);
+        $css = str_replace('; ', ';', $css);
+        $css = str_replace(': ', ':', $css);
+        $css = str_replace(' {', '{', $css);
+        $css = str_replace('{ ', '{', $css);
+        $css = str_replace(', ', ',', $css);
+        $css = str_replace('} ', '}', $css);
+        $css = str_replace(';}', '}', $css);
 
-		return trim( $css );
+        return trim($css);
     }
 
     protected static function moveCssImportToTop($str)
@@ -267,7 +269,6 @@ class FFToolsHelper {
 
         preg_match_all('/@import .*?;/', $str, $matches);
         if (!empty($matches[0])) {
-
             foreach ($matches[0] as $match) {
                 $str = str_replace($match, '', $str);
                 $import .= "$match\n";
@@ -345,7 +346,7 @@ class FFToolsHelper {
         $exclude = array_map('trim', $exclude);
 
         $doc = Factory::getDocument();
-        
+
         $styles = $doc->_styleSheets;
         $absUriPattern = '/^' . preg_quote(Uri::root(), '/') . '/';
         $relUriPattern = '/^' . preg_quote(Uri::root(true), '/') . '/';
@@ -371,7 +372,8 @@ class FFToolsHelper {
 
             if ((isset($style['options']['conditional']))
                 || ($startWithHttp && !$isAbsUrl)
-                || (!is_file(JPATH_ROOT . $file) && !$isAbsUrl)) {
+                || (!is_file(JPATH_ROOT . $file) && !$isAbsUrl)
+            ) {
                 if ($bundle) {
                     $chunk[] = $bundle;
                     $bundle = array();
@@ -388,5 +390,130 @@ class FFToolsHelper {
         }
 
         return $chunk;
+    }
+
+    public static function moveScriptToBottom($scripts, $inlineScripts)
+    {
+        $document = Factory::getDocument();
+        $app = Factory::getApplication();
+        $body = $app->getBody();
+
+        $lnEnd        = $document->_getLineEnd();
+        $tab          = $document->_getTab();
+        $buffer       = '';
+        $mediaVersion = $document->getMediaVersion();
+        $defaultJsMimes         = array('text/javascript', 'application/javascript', 'text/x-javascript', 'application/x-javascript');
+        $html5NoValueAttributes = array('defer', 'async');
+        // Generate script file links
+        foreach ($scripts as $src => $attribs) {
+            // Check if script uses IE conditional statements.
+            $conditional = isset($attribs['options']) && isset($attribs['options']['conditional']) ? $attribs['options']['conditional'] : null;
+
+            // Check if script uses media version.
+            if (
+                isset($attribs['options']['version']) && $attribs['options']['version'] && strpos($src, '?') === false
+                && ($mediaVersion || $attribs['options']['version'] !== 'auto')
+            ) {
+                $src .= '?' . ($attribs['options']['version'] === 'auto' ? $mediaVersion : $attribs['options']['version']);
+            }
+
+            $buffer .= $tab;
+
+            // This is for IE conditional statements support.
+            if (!is_null($conditional)) {
+                $buffer .= '<!--[if ' . $conditional . ']>';
+            }
+
+            $buffer .= '<script src="' . $src . '"';
+
+            // Add script tag attributes.
+            foreach ($attribs as $attrib => $value) {
+                // Don't add the 'options' attribute. This attribute is for internal use (version, conditional, etc).
+                if ($attrib === 'options') {
+                    continue;
+                }
+
+                // Don't add type attribute if document is HTML5 and it's a default mime type. 'mime' is for B/C.
+                if (in_array($attrib, array('type', 'mime')) && $document->isHtml5() && in_array($value, $defaultJsMimes)) {
+                    continue;
+                }
+
+                // B/C: If defer and async is false or empty don't render the attribute.
+                if (in_array($attrib, array('defer', 'async')) && !$value) {
+                    continue;
+                }
+
+                // Don't add type attribute if document is HTML5 and it's a default mime type. 'mime' is for B/C.
+                if ($attrib === 'mime') {
+                    $attrib = 'type';
+                }
+                // B/C defer and async can be set to yes when using the old method.
+                elseif (in_array($attrib, array('defer', 'async')) && $value === true) {
+                    $value = $attrib;
+                }
+
+                // Add attribute to script tag output.
+                $buffer .= ' ' . htmlspecialchars($attrib, ENT_COMPAT, 'UTF-8');
+
+                if (!($document->isHtml5() && in_array($attrib, $html5NoValueAttributes))) {
+                    // Json encode value if it's an array.
+                    $value = !is_scalar($value) ? json_encode($value) : $value;
+
+                    $buffer .= '="' . htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '"';
+                }
+            }
+
+            $buffer .= '></script>';
+
+            // This is for IE conditional statements support.
+            if (!is_null($conditional)) {
+                $buffer .= '<![endif]-->';
+            }
+
+            $buffer .= $lnEnd;
+        }
+
+        // Generate script declarations
+        foreach ($inlineScripts as $type => $content) {
+            $buffer .= $tab . '<script';
+
+            if (!is_null($type) && (!$document->isHtml5() || !in_array($type, $defaultJsMimes))) {
+                $buffer .= ' type="' . $type . '"';
+            }
+
+            $buffer .= '>' . $lnEnd;
+
+            // This is for full XHTML support.
+            if ($document->_mime != 'text/html') {
+                $buffer .= $tab . $tab . '//<![CDATA[' . $lnEnd;
+            }
+
+            $buffer .= $content . $lnEnd;
+
+            // See above note
+            if ($document->_mime != 'text/html') {
+                $buffer .= $tab . $tab . '//]]>' . $lnEnd;
+            }
+
+            $buffer .= $tab . '</script>' . $lnEnd;
+        }
+
+        $body = str_replace('</body>', $buffer . '</body>', $body);
+        $app->setBody($body);
+    }
+
+    public static function inlineCss($styleSheets)
+    {
+        $app = Factory::getApplication();
+        $body = $app->getBody();
+        foreach ($styleSheets as $value) {
+            $frags = explode('?', $value);
+            $file = JPATH_ROOT . $frags[0];
+            $content = "<style>\n" . file_get_contents($file) . "</style>";
+            $search = '<link href="'.$value.'" rel="stylesheet" />';
+            $body = str_replace($search, $content, $body);
+        }
+
+        $app->setBody($body);
     }
 }
